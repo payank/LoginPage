918af041dd5f93c7a3c6309e6a74baeee5585cb9
Payank Jain <payankjain@gmail.com>
2021-11-26 23:35:18 +0530

    commit

---
+++ src/Component/LoginPage.jsx
@@ -0,0 +1,82 @@
+import { Box, Typography, makeStyles, FormGroup, FormControl, InputLabel, Input, Button } from '@material-ui/core';
+import { useHistory, Redirect } from 'react-router-dom';
+//import { Redirect } from 'react-router'
+import { useState } from 'react';
+
+
+const initialValue = {
+    username: '',
+    password: '',
+    submitted: false,
+    loggedin: false 
+}
+
+const useStyles = makeStyles({
+    component: {
+        color: "#ff0000",
+    },
+    container: {
+        width: '50%',
+        margin: '5% 0 0 25%',
+        '& > *': {
+            marginTop: 20
+        }
+    }
+})
+
+const LoginPage = () => {
+    const classes = useStyles();
+    const [user, setUser] = useState(initialValue);
+    const { username, password, submitted, loggedin=true } = user;
+    let history = useHistory();
+
+    const onValueChange = (e) => {
+        console.log(e.target.value);
+        setUser({...user, [e.target.name]: e.target.value})
+    }
+
+    const validLogin = () => {
+        if (user.username === 'payank' && user.password === 'password') {
+            setUser({...user, loggedin: true, submitted: true});
+            localStorage.setItem('isLoggedin', true);
+            history.push('/all');
+            //<Redirect to="/all"/> // Here, nothings happens
+        } else {
+            console.log('Payank', user)
+            setUser({...user, loggedin: false, submitted: true});
+        }
+        
+    }
+
+
+    //box give deafault div
+    //typography gives default <p>
+    return (
+
+        <FormGroup className={classes.container}>
+            <Typography variant="h4">Login Page</Typography>
+            <FormControl>
+                <InputLabel htmlFor="my-input">Username</InputLabel>
+                <Input  type="text" onChange={(e) => onValueChange(e)} name='username' value={username} id="my-input" />
+                {submitted && !username &&
+                            <Box className={classes.component}>Username is required</Box>
+                }   
+            </FormControl>
+            <FormControl>
+                <InputLabel htmlFor="my-password">Password</InputLabel>
+                <Input type="password" onChange={(e) => onValueChange(e)} name='password' value={password} id="my-password"/>
+                {submitted && !password &&
+                            <Box className={classes.component}>Password is required</Box>
+                }   
+            </FormControl>
+            <FormControl>
+                <Button variant="contained" color="primary" onClick={() => validLogin()}>Login</Button>
+                {submitted && !loggedin &&
+                            <Box className={classes.component}>Invalid Credentials</Box>
+                }
+            </FormControl>
+        </FormGroup>
+    )
+}
+
+export default LoginPage;
---
+++ src/Component/NavBar.jsx
@@ -0,0 +1,30 @@
+import { AppBar, Toolbar, makeStyles } from '@material-ui/core';
+import { NavLink } from 'react-router-dom';
+
+const useStyle = makeStyles({
+    header: {
+        background: '#111111'
+    },
+    tabs: {
+        color: '#FFFFFF',   //color whir=te
+        marginRight: 20,
+        textDecoration: 'none', //remove underline
+        fontSize: 20
+    }
+})
+
+const NavBar = () => {
+
+    const classes = useStyle();
+    return (
+        <AppBar position="static" className={classes.header}>
+            <Toolbar>
+                <NavLink className={classes.tabs} to="/" replace exact>Login Page</NavLink>
+                <NavLink className={classes.tabs} to="/all" replace exact>All Users</NavLink>
+                <NavLink className={classes.tabs} to="/add" replace exact>Add User</NavLink>
+            </Toolbar>
+        </AppBar>
+    )
+}
+
+export default NavBar;
